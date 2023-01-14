8c7f68bd05e3b8ac2f9e6f3fbd392a884f3089c0
JackQuentinForde <jackforde96@gmail.com>
2023-01-14 17:10:25 +0000

    Created a basic skateboard controller

--- Scripts/CBSkateboard.gd
+++ Scripts/CBSkateboard.gd
@@ -1,31 +1,49 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+const ACCEL = 0.2
+const AIR_ACCEL = 0.1
+const MAX_SPEED = 4.0
+const RIDE_SPEED = 1.0
 const JUMP_VELOCITY = 4.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var speed = 0.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var direction = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
+	var accel = 0.0
+	
+	if is_on_floor():
+		accel = ACCEL
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		accel = AIR_ACCEL
+	
+	if direction > 0:
+		speed += accel
+	elif direction < 0:
+		speed -= accel
+	else:
+		if speed < 0:
+			speed += accel
+			if speed > 0:
+				speed = 0
+		elif speed > 0:
+			speed -= accel
+			if speed < 0:
+				speed = 0
+	
+	if speed > MAX_SPEED:
+		speed = MAX_SPEED
+	elif speed < -MAX_SPEED:
+		speed = -MAX_SPEED
+	
+	velocity.x = RIDE_SPEED
+	velocity.z = speed
 
 	move_and_slide()
