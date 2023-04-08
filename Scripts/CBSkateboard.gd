cb60f7fb63654440624fb33a0409ab9a70610503
JackQuentinForde <jackforde96@gmail.com>
2023-04-08 15:55:23 +0100

    Trying to get half pipes working

--- Scripts/CBSkateboard.gd
+++ Scripts/CBSkateboard.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 const ACCEL = 4.0
 const AIR_ACCEL = 2.0
 const RIDE_SPEED = 5.0
+const MAX_RIDE_SPEED = 5.0
 const MAX_TURN_SPEED = 2.0
 const JUMP_VELOCITY = 6.0
 const TRICK_SPEED = 15.0
@@ -21,14 +22,14 @@ var kickFlip = false
 var skateboard
 var defaultBoardRotation
 var animationPlayer
-var rayCast
+var rayCastFront
 
 func _ready():
 	skateboard = $Skateboard
 	defaultBoardRotation = skateboard.rotation
 	animationPlayer = get_node("Skateboard/AnimationPlayer")
 	animationPlayer.set_assigned_animation("Ollie")
-	rayCast = $RayCast3D
+	rayCastFront = $RayCastFront
 
 func _physics_process(delta):
 	setAcceleration()
@@ -90,15 +91,14 @@ func doAKickFlip(delta):
 		kickFlip = false
 		
 func moveLogic():
-	var vy = velocity.y
-	velocity = Vector3.ZERO
-	var normal = rayCast.get_collision_normal()
-#	if not is_on_floor() and normal != Vector3.UP:
-#		normal = Vector3.UP
+	var normal = rayCastFront.get_collision_normal()
 	var xForm = alignWithY(global_transform, normal)
 	global_transform = global_transform.interpolate_with(xForm, 0.2)
-	velocity += -transform.basis.z * RIDE_SPEED
-	velocity.y = vy
+	var heading = -transform.basis.z * RIDE_SPEED
+	velocity.x = heading.x
+	velocity.z = heading.z
+	if is_on_floor():
+		velocity.y += heading.y
 	
 func alignWithY(xForm, newY):
 	xForm.basis.y = newY
